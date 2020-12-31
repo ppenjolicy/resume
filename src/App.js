58be1a56aaa7232c26dbb8cdab74a9fae9070b8a
ppenjolicy <yurin.pk.123@gmail.com>
2020-12-31 23:00:04 +0700

    edit container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Container, Grid } from '@material-ui/core'
+import Profile from './components/Profile'
+import Experience from './components/Experience'
 
 function App() {
   return (
-    <div className="App">
-      <h1>TEST</h1>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3} style={{background: 'red'}}>
+          <Profile/>
+        </Grid>
+        <Grid item xs style={{background: 'blue'}}>
+          <Experience/>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
