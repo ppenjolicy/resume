12352638f5d2fd6047481ae42ec92acf58be8a42
ppenjolicy <yurin.pk.123@gmail.com>
2021-01-03 23:03:25 +0700

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,52 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import React  from 'react'
 import Self from '../components/selfpic3.png'
 import './Profile.css'
+import RubberBand from 'react-reveal/RubberBand';
+import CallIcon from '@material-ui/icons/Call';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
-export default function Profile() {
+const Profile = () => {
+
     return (
-        <div className='root'>
+        <div className='box'>
 
             <figure className='self-pic'>
                 <img src={Self} alt="" />
             </figure>
 
-            <h1>
-                Pailin Parkpitcharoen
-            </h1>
+            <div className = "textbox">
+                <h1>
+                    <RubberBand>
+                            P A I L I N <br/>
+                            P A R K P I T C H A R O E N
+                    </RubberBand>
+                </h1>
+            </div>
+            
+            <div>
+                <ul>
+                    <li>
+                            <CallIcon className="iconbox"/>
+                            <p className='texticon'>062-759-5542</p>
+                    </li>
+                    <li>
+                            <MailOutlineIcon className='iconbox'/>
+                            <p className='texticon'>pparkpitcharoen@gmail.com</p>
+                    </li>
+                    <li>
+                            <GitHubIcon className='iconbox'/>
+                            <a href="https://github.com/ppenjolicy" className='texticon'>ppenjolicy</a>
+                    </li>
+                    <li>
+                            <FacebookIcon className='iconbox'/>
+                            <a href="https://www.facebook.com/JoyParkpitcharoen" className='texticon'>Jolicy Ppen</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
 
+export default Profile;
